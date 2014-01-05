323fb2a07940738a6f6345765613e1fc6c52da1f
yr1216 <wangshuai@wangshuais-MacBook-Pro.local>
2014-01-05 18:19:50 +0000

    Initial Commit

---
+++ twitterchat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  twitterchat
+//
+//  Created by wangshuai on 05/01/2014.
+//  Copyright (c) 2014 wangshuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ twitterchat/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  twitterchat
+//
+//  Created by wangshuai on 05/01/2014.
+//  Copyright (c) 2014 wangshuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
